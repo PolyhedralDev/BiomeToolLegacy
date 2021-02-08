cde453d0a131481e93633c3260b77001506b95e3
dfsek <dfsek@protonmail.com>
2021-02-07 18:40:55 -0700

    Add everything

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # BiomeTool
 Utility for viewing biome pipelines
+
+This utility application allows viewing large-scale images of [Terra](https://github.com/PolyhedralDev/Terra)
+Biome Pipelines. It is a very helpful utility for rapid development of Terra configs.
+
+## Download
+Download the latest version from the Releases tab of this GitHub repository.
+
+## Usage
+To use the program, simply place it in a folder, and put your *entire* config pack in a
+subdirectory named `pack` or in a ZIP file named `pack.yml`. Run the program and a map of
+biomes will be generated based on your pack's pipeline. Biomes will be colored based on the
+`color` key in the pack config.
+
+Press `s` to randomize seed/reload the pipeline.
+
+Click anywhere on the application window to view the biome name at that location.
+
+![](./images/window.png)
---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow").version("6.1.0")
+}
+
+group = "com.dfsek"
+version = "0.1.0"
+
+repositories {
+    maven { url = uri("https://repo.codemc.org/repository/maven-public") }
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+    implementation("com.dfsek:Tectonic:1.2.3")
+    implementation("com.dfsek.terra.common:common:4.0.1-BETA+f8cf61e2")
+    implementation("org.yaml:snakeyaml:1.27")
+    implementation("commons-io:commons-io:2.8.0")
+    implementation("net.jafama:jafama:2.3.2")
+    implementation("com.googlecode.json-simple:json-simple:1.1")
+    implementation("com.google.guava:guava:30.0-jre")
+}
+
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "com.dfsek.biome.BiomeTool"
+    }
+}
